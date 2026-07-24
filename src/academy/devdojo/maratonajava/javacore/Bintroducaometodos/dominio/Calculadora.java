67e08499001050a604b0e74429d3c77792b68b29
Cleysson Santos <149117307+cleysson-santos@users.noreply.github.com>
2026-07-24 20:16:25 -0300

    metodos com retorno

--- src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
@@ -12,4 +12,27 @@ public class Calculadora {
     public void multiplicaDoisNumeros(int num1, float num2) {
         System.out.println(num1 * num2);
     }
+
+    public double divideDoisNumeros(double num1, double num2) {
+        if(num2 == 0){
+            return 0;
+        }
+        return num1 / num2;
+    }
+
+    public void imprimeDivisaoDeDoisNumeros(double num1, double num2) {
+        if(num2 == 0) {
+            System.out.println("Não existe divisão por zero");
+            return;
+        }
+        System.out.println(num1 / num2);
+    }
+
+    public void alteraDoisNumeros(int num1, int num2) {
+        num1 = 99;
+        num2 = 33;
+        System.out.println("Dentro do alteraDoisNumeros");
+        System.out.println("Num1 " + num1);
+        System.out.println("Num2 " + num2);
+    }
 }
