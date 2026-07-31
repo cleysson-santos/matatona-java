78e223c88bf5b6c113235caa6dc2c699e4548bd2
Cleysson Santos <149117307+cleysson-santos@users.noreply.github.com>
2026-07-31 20:33:32 -0300

    modificador de acesso private, get e set

--- src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Funcionario.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Funcionario.java
@@ -1,20 +1,20 @@
 package academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio;
 
 public class Funcionario {
-    public String nome;
-    public int idade = 15;
-    public double[] salarios;
+    private String nome;
+    private int idade;
+    private double[] salarios;
+    private double media;
 
     public void imprime(){
         System.out.println(this.nome);
         System.out.println(this.idade);
-        if(salarios != null) {
+        if(salarios == null) {
             return;
         }
             for(double salario: salarios) {
                 System.out.print(salario + " ");
             }
-
 
         imprimeMediaSalarial();
     }
@@ -23,13 +23,39 @@ public class Funcionario {
         if(salarios == null) {
             return;
         }
-
-        double media = 0;
         for (double salario : salarios) {
             media += salario;
         }
         media /= salarios.length;
 
         System.out.println("\nMédia salarial " + media);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public double[] getSalarios() {
+        return salarios;
+    }
+
+    public void setSalarios(double[] salarios) {
+        this.salarios = salarios;
+    }
+
+    public double getMedia() {
+        return media;
     }
 }
