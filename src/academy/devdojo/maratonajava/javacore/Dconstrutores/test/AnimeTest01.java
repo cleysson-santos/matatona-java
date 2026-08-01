d6d52e0a78fb34790184591f0a1efa87e444b9ee
Cleysson Santos <149117307+cleysson-santos@users.noreply.github.com>
2026-08-01 16:52:25 -0300

    sobrecarga construtores

--- src/academy/devdojo/maratonajava/javacore/Dconstrutores/test/AnimeTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Dconstrutores/test/AnimeTest01.java
@@ -4,7 +4,7 @@ import academy.devdojo.maratonajava.javacore.Dconstrutores.dominio.Anime;
 
 public class AnimeTest01 {
     public static void main(String[] args) {
-        Anime anime = new Anime("Haikyuu", "TV", 15, "Esporte");
+        Anime anime = new Anime("Haikyuu", "TV", 15, "Esporte", "Production IG");
         anime.imprime();
     }
 
